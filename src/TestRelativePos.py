15f95920a49511629299b5b4da5c25e8b32b4534
ShiroTsuma-github <spam.shiro@gmail.com>
2023-09-07 23:42:59 +0200

    added option to bind to point

--- src/TestRelativePos.py
+++ src/TestRelativePos.py
@@ -7,14 +7,48 @@ running = True
 
 
 class Square(pygame.sprite.Sprite):
-    def __init__(self, color, width, height, x, y, parent: 'Square'):
+    def __init__(self,
+                 color,
+                 width, height,
+                 x, y,
+                 parent: 'Square',
+                 relation_point='NW',
+                 image=None):
         pygame.sprite.Sprite.__init__(self)
-        self.image = pygame.Surface([width, height])
-        self.image.fill(color)
+        self.image = self.set_image(image, width, height, color)
         self.rect = self.image.get_rect()
         self.parent = parent
         self.rect.x, self.rect.y = x, y
+        self.offset_x, self.offset_y = 0, 0
+        self.set_relation_point(relation_point)
         self.set_relative_to_parent()
+
+    def set_image(self, image, width, height, color):
+        if image is None:
+            result = pygame.Surface([width, height])
+            result.fill(color)
+            return result
+        result = pygame.image.load(image)
+        result = pygame.transform.scale(result, (width, height))
+        return result
+
+    def set_relation_point(self, relation_point):
+        if self.parent is None:
+            self.offset_x, self.offset_y = 0, 0
+            return
+        coordinates = {
+            'NW': (0, 0),
+            'N': (self.parent.rect.width / 2, 0),
+            'NE': (self.parent.rect.width, 0),
+            'E': (self.parent.rect.width, self.parent.rect.height / 2),
+            'SE': (self.parent.rect.width, self.parent.rect.height),
+            'S': (self.parent.rect.width / 2, self.parent.rect.height),
+            'SW': (0, self.parent.rect.height),
+            'W': (0, self.parent.rect.height / 2),
+            'C': (self.parent.rect.width / 2, self.parent.rect.height / 2),
+        }
+        self.offset_x = coordinates.get(relation_point, 0)[0]
+        self.offset_y = coordinates.get(relation_point, 0)[1]
 
     def clamp(self, n: int, min: int, max: int, _bool=False) -> int:
         if n < min:
@@ -30,15 +64,15 @@ class Square(pygame.sprite.Sprite):
                 return True
             return n
 
-    def set_relative_to_parent(self):
+    def set_relative_to_parent(self) -> None:
         if self.parent is None:
             return
         if self.is_inside_parent():
             return
-        self.rect.x = self.rect.x + self.parent.rect.x
-        self.rect.y = self.rect.y + self.parent.rect.y
+        self.rect.x = self.rect.x + self.parent.rect.x + self.offset_x
+        self.rect.y = self.rect.y + self.parent.rect.y + self.offset_y
 
-    def is_inside_parent(self):
+    def is_inside_parent(self) -> bool:
         if self.parent is None:
             if (self.rect.x + self.rect.width in range(0, screen.get_width())) and\
                (self.rect.y + self.rect.height in range(0, screen.get_height())):
@@ -51,7 +85,7 @@ class Square(pygame.sprite.Sprite):
             return False
         return True
 
-    def get_relative_to_parent(self):
+    def get_relative_to_parent(self) -> tuple[int, int]:
         if self.parent is not None:
             return self.rect.x - self.parent.rect.x, self.rect.y - self.parent.rect.y
         else:
@@ -70,6 +104,10 @@ class Square(pygame.sprite.Sprite):
     def bound_move(self, x, y):
         if self.parent is None:
             raise Exception("Cannot bound move without parent")
+        elif self.offset_x + self.rect.width > self.parent.rect.width:
+            raise Exception("Cannot bound_move. Relation point + width > parent width")
+        elif self.offset_y + self.rect.height > self.parent.rect.height:
+            raise Exception("Cannot bound_move. Relation point + width > parent width")
         max_x = self.parent.rect.width - self.rect.width
         max_y = self.parent.rect.height - self.rect.height
         self.rect.x += x
@@ -100,17 +138,17 @@ class Square(pygame.sprite.Sprite):
 
 all_sprites = pygame.sprite.Group()
 parent = Square("red", 500, 500, 100, 100, None)
-child = Square("blue", 400, 400, 0, 0, parent)
+child = Square("blue", 400, 400, 0, 0, parent, relation_point='NE')
 child2 = Square("green", 300, 300, 50, 0, child)
-child3 = Square("yellow", 200, 200, 50, 0, child2)
-child4 = Square("orange", 100, 100, 50, 0, child3)
-child5 = Square("pink", 50, 50, 50, 0, child4)
+child3 = Square("yellow", 200, 150, 50, 0, child2)
+child4 = Square("orange", 100, 100, 50, 0, child3, image='resources/images/enemy.png')
+# child5 = Square("pink", 50, 50, 50, 0, child4, )
 all_sprites.add(parent)
 all_sprites.add(child)
 all_sprites.add(child2)
-all_sprites.add(child3)
-all_sprites.add(child4)
-all_sprites.add(child5)
+# all_sprites.add(child3)
+# all_sprites.add(child4)
+# all_sprites.add(child5)
 
 
 while running:
@@ -124,11 +162,12 @@ while running:
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("purple")
     all_sprites.draw(screen)
-    child.bound_move(randint(-3, 3), randint(-3, 3))
+    
+    # child.bound_move(randint(-3, 3), randint(-3, 3))
     child2.bound_move(randint(-3, 5), randint(-3, 3))
-    child3.bound_move(randint(-3, 3), randint(-3, 3))
-    child4.bound_move(randint(-3, 3), randint(-3, 3))
-    child5.place(300, 10)
+    # child3.bound_move(randint(-3, 3), randint(-3, 3))
+    # child4.bound_stick(0, 20)
+    # child5.place(300, 10)
     all_sprites.update(events)
 
     # RENDER YOUR GAME HERE
